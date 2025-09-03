b43d4cd7c0d2e2bfb024e393b252513bead894b5
Baranin67 <mati.baranowicz@gmail.com>
2025-09-03 18:52:32 +0200

    fix: button and modal

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,38 +1,34 @@
-'use client';
+"use client";
 
-import { useRef } from 'react';
-import { useButton } from 'react-aria';
-import { useFormStatus } from 'react-dom';
+import { useRef } from "react";
+import { useButton } from "react-aria";
 
-import styles from './styles.module.scss';
-import { ButtonProps } from './types';
+import styles from "./styles.module.scss";
+import { ButtonProps } from "./types";
 
 export default function Button({
-	variant,
-	scheme = 'dark',
-	icon,
-	iconPos = 'left',
-	children,
-	...props
+  variant,
+  scheme = "dark",
+  icon,
+  iconPos = "left",
+  children,
+  ...props
 }: ButtonProps) {
-	const { pending } = useFormStatus();
+  const ref = useRef<HTMLButtonElement>(null);
+  const { buttonProps } = useButton(props, ref);
 
-	const ref = useRef<HTMLButtonElement>(null);
-	const { buttonProps } = useButton(props, ref);
-
-	return (
-		<button
-			className={styles.wrapper}
-			data-variant={variant}
-			data-scheme={scheme}
-			ref={ref}
-			disabled={pending}
-			{...buttonProps}
-			{...props}
-		>
-			{iconPos === 'left' && icon}
-			{children}
-			{iconPos === 'right' && icon}
-		</button>
-	);
+  return (
+    <button
+      className={styles.wrapper}
+      data-variant={variant}
+      data-scheme={scheme}
+      ref={ref}
+      {...buttonProps}
+      {...props}
+    >
+      {iconPos === "left" && icon}
+      {children}
+      {iconPos === "right" && icon}
+    </button>
+  );
 }
--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,50 +1,51 @@
-'use client';
+"use client";
 
-import { useRef } from 'react';
-import { useModalOverlay } from 'react-aria';
+import { useRef } from "react";
+import { useModalOverlay } from "react-aria";
 
-import styles from './styles.module.scss';
-import { ModalProps } from './types';
+import styles from "./styles.module.scss";
+import { ModalProps } from "./types";
 
-import { useLanguageState } from '@/context/language';
-import Button from '@/components/button';
-import { CrossIcon } from '../icons';
+import { useLanguageState } from "@/context/language";
+import Button from "@/components/button";
+import { CrossIcon } from "../icons";
 
 export default function Modal({
-	state,
-	buttons,
-	cancelButton,
-	children,
-	title,
-	...props
+  state,
+  buttons,
+  cancelButton,
+  children,
+  title,
+  ...props
 }: ModalProps) {
-	const ref = useRef<HTMLInputElement>(null);
-	const { underlayProps, modalProps } = useModalOverlay(props, state, ref);
-	const { currentLanguageFile: lang } = useLanguageState();
+  const ref = useRef<HTMLInputElement>(null);
+  const { underlayProps, modalProps } = useModalOverlay(props, state, ref);
+  const { currentLanguageFile: lang } = useLanguageState();
 
-	return (
-		<div className={styles.underlay} {...underlayProps}>
-			<div className={styles.modal}>
-				<div className={styles.content} {...modalProps}>
-					<header>
-						<h2>{title}</h2>
-					</header>
-					{children}
-				</div>
+  return (
+    <div className={styles.underlay} {...underlayProps}>
+      <div className={styles.modal}>
+        <div className={styles.content} {...modalProps}>
+          <header>
+            <h2>{title}</h2>
+          </header>
+          {children}
+        </div>
 
-				<div className={styles.buttons}>
-					{/* BUTTONS */}
-					{buttons && buttons.map((btn, idx) => <Button key={idx} {...btn} />)}
-					{/* CANCEL BUTTON */}
-					<Button
-						variant="outline"
-						children={lang?.common?.cancel}
-						icon={<CrossIcon />}
-						onClick={_ => state.close()}
-						{...cancelButton}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+        <div className={styles.buttons}>
+          {/* BUTTONS */}
+          {buttons && buttons.map((btn, idx) => <Button key={idx} {...btn} />)}
+          {/* CANCEL BUTTON */}
+          <Button
+            variant="outline"
+            icon={<CrossIcon />}
+            onClick={(_) => state.close()}
+            {...cancelButton}
+          >
+            {lang?.common?.cancel}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
